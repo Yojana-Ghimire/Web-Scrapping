391d2a4fc6d7b06369ce17ccbba20008c4aaa2a2
Yojana Ghimire <101090327+codertheplan@users.noreply.github.com>
2024-06-24 21:01:43 +0545

    Add files via upload

--- agencies_data.py
+++ agencies_data.py
@@ -8,6 +8,21 @@ with open('data.json', 'w') as f:
 
 with open('data.json', 'r') as f:
     data = json.load(f)
+def read_json_file(data):
+    with open('data.json', 'r') as f:
+        data = json.load(f)
+        return data
 
+def analyze_data(data):
+    print(data)
+
+def main():
+    filename = 'data.json'
+    data = read_json_file(filename)
+    if data:
+        analyze_data(data)
+
+if __name__ == '__main__':
+    main()
 
 print(data)
