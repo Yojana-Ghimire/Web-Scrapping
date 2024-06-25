4e9e7f4cf87e8fb48b2eb0fd6d627656c3fe76fe
Yojana Ghimire <101090327+codertheplan@users.noreply.github.com>
2024-06-25 20:50:21 +0545

    Add files via upload

--- agencies_data.py
+++ agencies_data.py
@@ -1,28 +1,29 @@
 import requests
 import json
-res=requests.get("https://auditcity.io/api/data/e7eba98ba5b85dbc98b8def5319ef4c3")
-print(res)
-data=res.json()
-with open('data.json', 'w') as f:
-    json.dump(data, f, indent=4)
 
-with open('data.json', 'r') as f:
-    data = json.load(f)
+
 def read_json_file(data):
-    with open('data.json', 'r') as f:
-        data = json.load(f)
-        return data
-
+    try:
+        with open('data.json', 'r') as f:
+            data = json.load(f)
+            return data
+    except FileNotFoundError:
+        print(f"File '{data.json}' not found.")
+        return None
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON from '{data.json}': {e}")
+        return None
 def analyze_data(data):
-    print(data)
+    if data:
+        print("Analyzing JSON data:")
+        for key, value in data.items():
+            print(f"{key}: {value}")
+    else:
+        print("No data to analyze.")
 
 def main():
     filename = 'data.json'
     data = read_json_file(filename)
-    if data:
-        analyze_data(data)
 
 if __name__ == '__main__':
     main()
-
-print(data)
