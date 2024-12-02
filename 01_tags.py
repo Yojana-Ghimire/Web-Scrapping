8c000bbdf49e37a57b0d9e2c4930ca67a4f93636
codertheplan <yojanaghimire5@gmail.com>
2024-12-02 21:56:52 +0545

    Second commit-modification

--- 01_tags.py
+++ 01_tags.py
@@ -8,3 +8,14 @@ print(soup.prettify(),type(soup.title.string))
 print(soup.title.string)
 print(soup.div)
 print(soup.find_all("div"))
+#print(soup.select("div.italic"))
+print(soup.findAll(class_="italic"))
+#i=0
+#for parent in soup.find(class_="yojana").parents:
+ #   i+=1
+    #print(parent)
+    #if(i==2):
+     #   break
+
+cont = soup.find(class_="container") 
+print(cont)   
