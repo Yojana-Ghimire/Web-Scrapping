7c849b7ec33dca8311bf233974199ed0eaec10fc
codertheplan <yojanaghimire5@gmail.com>
2024-12-03 07:31:51 +0545

    Third commit: append litag and ultag

--- 01_tags.py
+++ 01_tags.py
@@ -17,5 +17,15 @@ print(soup.findAll(class_="italic"))
     #if(i==2):
      #   break
 
-cont = soup.find(class_="container") 
-print(cont)   
+#cont = soup.find(class_="container") 
+#print(cont)  
+
+ulTag= soup.new_tag("ul")
+ulTag.string = "Data"
+liTag=soup.new_tag("li")
+liTag.string ="Home"
+ulTag.append(liTag)
+
+soup.html.body.insert(0,ulTag)
+with open("modified.html",'w') as f:
+    f.write(str(soup))
