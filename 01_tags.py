d33e3e9111e46f6eb369826ee62d800a29788545
codertheplan <yojanaghimire5@gmail.com>
2024-12-03 07:38:55 +0545

    4th commit:check if it has attributes or not

--- 01_tags.py
+++ 01_tags.py
@@ -20,12 +20,15 @@ print(soup.findAll(class_="italic"))
 #cont = soup.find(class_="container") 
 #print(cont)  
 
-ulTag= soup.new_tag("ul")
-ulTag.string = "Data"
-liTag=soup.new_tag("li")
-liTag.string ="Home"
-ulTag.append(liTag)
+#ulTag= soup.new_tag("ul")
+#ulTag.string = "Data"
+#liTag=soup.new_tag("li")
+#liTag.string ="Home"
+#ulTag.append(liTag)
 
-soup.html.body.insert(0,ulTag)
-with open("modified.html",'w') as f:
-    f.write(str(soup))
+#soup.html.body.insert(0,ulTag)
+#with open("modified.html",'w') as f:
+ #   f.write(str(soup))
+
+cont = soup.find(class_="container")
+print(cont.has_attr("yojana"))
